babc424c859d4923f55d812959250cd20e6b65c4
unknown <akkamith@gmail.com>
2022-01-11 13:25:14 +0530

    I Created My Block App

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog App</title>
+    <link rel="stylesheet" href="./Plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="./CSS/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet"> 
+    <script src="./Plugins/jquery.min.js"></script>
+    <script src="./Plugins/bootstrap.min.js"></script>
+</head>
+<body>
+     <?php include_once('./PHP/navbar.php')?>
+     <div class="container">
+         <div class="row">
+             <div class="col-md-12">
+                 <div class="jumbotron mt-4">
+                    <h4 id="jumbo-header">Welcome to the Kamiya Learning</h4>
+                    <h4 id="jumbo-emoji">😎🎓😎</h4>
+                 </div>
+             </div>
+         </div>
+     </div>
+</body>
+</html>
